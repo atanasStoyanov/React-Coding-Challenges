d616e16e9395a145b752115517d441477f106480
atanasStoyanov <atanas.stoyanov1993@gmail.com>
2022-12-21 16:23:54 +0200

    Circle click challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import { BrowserTreeComponent } from "./BrowserTreeComponent";
+import { CircleClick } from "./CircleClick";
 
 function App() {
   return (
     <div className="App">
       <BrowserTreeComponent />
+      {/* <CircleClick /> */}
     </div>
   );
 }
--- src/BrowserTreeComponent/BrowserTreeComponents.tsx
+++ src/BrowserTreeComponent/BrowserTreeComponents.tsx
@@ -1,11 +1,12 @@
+import "./BrowserTreeComponent.css"
 import { tree } from "./treeMock";
 import { Entry } from './Entry'
 
 export const BrowserTreeComponent = () => {
   return (
     <div>
-      {tree.children.map((entry) => (
-        <Entry entry={entry} depth={1} />
+      {tree.children.map((entry, index) => (
+        <Entry key={index} entry={entry} depth={1} />
       ))}
     </div>
   );
